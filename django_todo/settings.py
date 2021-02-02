ed1956a883e23b4382b681800a85540ad587d830
NikolasKotsikos <kotsikn@gmail.com>
2021-02-02 16:50:54 +0000

    Fixed secret key issue,added model

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vt9l5gm7(uf)3196y62c8xugdrkvw(_ld6a-3_6kx9wh25!^k5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
